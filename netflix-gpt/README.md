b8ec9cf4bb5eb42582a2d80cb27ca2f739561c01
Radhika <rradhika393@gmail.com>
2024-04-28 18:54:36 +0530

    Basic layout for sign in page.

--- netflix-gpt/README.md
+++ netflix-gpt/README.md
@@ -1,4 +1,22 @@
 # Netflix GPT
 
--Create react app
--Configured tailwind css
+- Create react app
+- Configured tailwind css
+- Header
+- Login form
+
+# Features:
+
+- Login/signup page
+  - signIn form
+  - Redirect to browse page
+- Browse page (after authentication)
+  - Header
+  - Main movie
+    - Trailor in background
+    - Title and description
+    - Movie suggestions
+      - Movie list \* n
+- NetflixGPT
+  - Search bar
+  - Movie suggestions
---
+++ netflix-gpt/src/components/Login.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Header from "./Header";
+
+const Login = () => {
+  return (
+    <div>
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/058eee37-6c24-403a-95bd-7d85d3260ae1/e10ba8a6-b96a-4308-bee4-76fab1ebd6ca/IN-en-20240422-POP_SIGNUP_TWO_WEEKS-perspective_WEB_db9348f2-4d68-4934-b495-6d9d1be5917e_small.jpg"
+          alt="background-img"
+        />
+      </div>
+      <form className="absolute p-12 bg-black w-1/4 my-36 mx-auto right-0 left-0 text-white bg-opacity-80">
+        <h1 className="font-bold text-3xl py-4">Sign In</h1>
+        <input
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-4 w-full bg-gray-700 rounded-lg"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-gray-700 rounded-lg"
+        />
+        <button className="p-4 my-6 w-full bg-red-700 rounded-lg">
+          Sign In
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
